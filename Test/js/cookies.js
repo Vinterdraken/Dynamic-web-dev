7ddc8a42e9dc15f92e96ee8d8315c83884ed9246
MATEO RAT (1715232) <1715232@rgu.ac.uk>
2018-02-15 10:09:44 +0000

    [JS test] cooookies

--- Test/js/cookies.js
+++ Test/js/cookies.js
@@ -17,3 +17,15 @@ function reconizeUser(){
   var username = document.cookie;
 
 }
+
+function checkCookie() {
+    var username = getCookie("username");
+    if (username != "") {
+        alert("Welcome again " + username);
+    } else {
+        username = prompt("Please enter your name:", "");
+        if (username != "" && username != null) {
+            setCookie("username", username, 365);
+        }
+    }
+}
