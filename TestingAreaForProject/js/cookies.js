8060ccae4b4f153803edf5309f86f4af3c0b2e95
Vinterdraken <mateo.rat01@gmail.com>
2018-03-08 09:36:37 +0000

    [Project] cookie clean code

--- TestingAreaForProject/js/cookies.js
+++ TestingAreaForProject/js/cookies.js
@@ -1,24 +1,24 @@
 
 /**
 * This function is creating a cookie
-* cName is the name of the cookie
-* cValue is the value stored by the cookie
+* cookieName is the name of the cookie
+* cookieValue is the value stored by the cookie
 * exdays is the number of day that the cookie will stay alive :3
 */
-function setCookie(cName, cValue, exdays) {
+function setCookie(cookieName, cookieValue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+ d.toUTCString();
-    cValueInLower = cValue.toLowerCase();    
-    document.cookie = cName + "=" + cValueInLower + ";" + expires + "; Path=/";
+    cookieValueInLower = cookieValue.toLowerCase();    
+    document.cookie = cookieName + "=" + cookieValueInLower + ";" + expires + "; Path=/";
 }
 
 /**
 * This function allow us to get the value of a cookie
-* cName is the name of the cookie that we want the value from
+* cookieName is the name of the cookie that we want the value from
 */
-function getCookie(cName) {
-    var name = cName + "=";
+function getCookie(cookieName) {
+    var name = cookieName + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
     var ca = decodedCookie.split(';');
     for(var i = 0; i <ca.length; i++) {
@@ -38,16 +38,13 @@ function getCookie(cName) {
 * if the cookies are existing then it will ask if the user still in the last country stored 
 */
 function checkCookies() {
+	
     var usernameCookie = getCookie("username");
     var countryCookie = getCookie("country");
     var tutorialCookie = getCookie("tutorial");
 
-    console.log(usernameCookie);
-    console.log(countryCookie);
-    console.log(tutorialCookie);
-
     if (usernameCookie != "" && countryCookie != "" && tutorialCookie == "done") {
-        var newCountry = prompt("Hi " + usernameCookie + "! It's good to see you again. :D \nLast time you told me you where in " + countryCookie + ". \nPlease if you're not there anymore, let me know where you are now :)", countryCookie);
+        var newCountry = prompt("Hi " + usernameCookie + "! It's good to see you again. :D \nLast time you told me you were in " + countryCookie + ". \nPlease if you're not there anymore, let me know where you are now :)", countryCookie);
         setCookie("country", newCountry, 365);
     } 
     else if(usernameCookie != "" && countryCookie != "" && tutorialCookie == "todo"){
@@ -58,7 +55,7 @@ function checkCookies() {
         var username = prompt("Hi there ! I'm the Culture Web App, what's your name ? :D (You can use a pseudo if you want)", "");
         var country = prompt("I'd like to know your location, this way I'll can make your experience with me better ;)", "");
 
-        if (username != "" && username != null && country != "" && country != null) {
+        if (username != "" && country != "") {
             setCookie("username", username, 365);
             setCookie("country", country, 365);
             setCookie("tutorial", "todo", 365);             
